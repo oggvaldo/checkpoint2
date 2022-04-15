50db4072061c1c97a941529044bbc270510b992e
gabazevdo <gbldja@gmail.com>
2022-04-15 00:00:41 -0300

    Comentarios e organização do código

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
-// Botão para mudar o tema
-
+///////////////////// DARKTHEME
 const html = document.querySelector("html");
 const checkbox = document.querySelector("#checkbox");
 
@@ -7,40 +6,46 @@ checkbox.addEventListener("change", () => {
   html.classList.toggle("dark-mode");
 });
 
-// Botão modal
-
+///////////////////// MODAL
 let btn = document.getElementById("btn");
 let span = document.getElementsByClassName("close")[0];
 
-
+// Função para aparecer o modal
 function openModal(mn) {
   let modal = document.getElementById(mn);
-  
+
   if (typeof modal == "undefined" || modal === null) return;
 
   modal.style.display = "Block";
-  
-  // Quando clicar a tecla 'esc' do teclado fecha o modal
-window.addEventListener("keydown", function (e) {
-  if (e.keyCode == 27 && modal.style.display == "block") {
-    modal.style.display = "none";
-  }
-});
+  //Ao clicar no botão, o body ficará sem a barra de rolagem:
+  document.body.style.overflow = "hidden";
 
-window.addEventListener("click", function (event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-    document.body.style.overflow = "auto";
-  }
-});
 
-  // Quando o usuário clica em (x) o modal fica display: none
-  span.addEventListener("click", function () {
-    modal.style.display = "none";
-    document.body.style.overflow = "auto";
+  // Ao clicar fora do modal ele fica com display: none
+  window.addEventListener("click", function (e) {
+    if (e.target == modal) {
+      modal.style.display = "none";
+      document.body.style.overflow = "auto";
+    }
   });
 
 
+  // Quando clica em (x) o modal fica com display: none
+  span.addEventListener("click", function () {
+    modal.style.display = "none";
+    document.body.style.overflow = "auto";
+
+  });
+
+  // Quando clicar a tecla 'esc' do teclado fecha o modal
+  window.addEventListener("keydown", function (e) {
+    if (e.keyCode == 27 && modal.style.display == "block") {
+      modal.style.display = "none";
+      document.body.style.overflow = "auto";
+    }
+  });
 }
 
-// Link do modal: https://codepen.io/chomikoo/pen/oWWapq
+// Inspirações para o modal:
+// https://codepen.io/chomikoo/pen/oWWapq
+// https://www.youtube.com/watch?v=NXdjf6GJSKo&ab_channel=SatellaSoft
